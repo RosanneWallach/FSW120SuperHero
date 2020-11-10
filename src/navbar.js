e79f62b1d644be1c8182f98e53090713da4eeb0a
Rosanne Wallach <66342005+RosanneWallach@users.noreply.github.com>
2020-11-10 02:45:21 -0500

    Add files via upload

---
+++ src/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav className="nav-container">
+            <h1>Super Hero Page</h1>
+        </nav>
+    )
+}
+
+export default Navbar
